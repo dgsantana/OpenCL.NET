6a4de240aa96e5f0764d989c0e2c7bc53121e521
Adam C <coding1729@gmail.com>
2020-09-21 04:38:53 -0700

    Fix kernel test

--- OpenCL.NetCore.Tests/KernelWrapperGenerationTests.cs
+++ OpenCL.NetCore.Tests/KernelWrapperGenerationTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
+﻿using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace OpenCL.NetCore.Tests
@@ -13,9 +9,10 @@ namespace OpenCL.NetCore.Tests
         [TestMethod]
         public void TestKernelGeneration()
         {
-            var basePath = AppDomain.CurrentDomain.BaseDirectory;
-            var filePath = Path.Combine(basePath, @"TestKernels\TestKernel1.cl");
-            var wrapper = OpenCL.NetCore.Tasks.Kernel.ProcessKernelFile(filePath, File.ReadAllText(filePath));
+            //var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            //var filePath = Path.Combine(basePath, @"TestKernels\TestKernel1.cl");
+            string filePath = @"E:\repos\OpenCL.NetCore\OpenCL.NetCore.Tests\TestKernels\TestKernel1.cl";
+            Tasks.Kernel.ProcessKernelFile(filePath, File.ReadAllText(filePath));
         }
     }
 }
